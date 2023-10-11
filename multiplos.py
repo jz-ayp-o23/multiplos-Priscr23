c2f22a7f19b4a122e68f1256a487e18122e541d2
Priscr23 <Priscila.cervantes@iteso.mx>
2023-10-11 17:24:08 -0600

    1

--- multiplos.py
+++ multiplos.py
@@ -1,15 +1,16 @@
 """
-Inserta el encabezado aquí y escribe tu código abajo
+Diseñar un programa para que, dados dos números enteros, determine si uno es múltiplo del otro.
+750722
 """
 
-# Declaraciones
-CONSTANTE = valor
+#Entrada
+numero_1 = int(input("Introdusca un número: "))
+numero_2 = int(input("Introduzca otro número: "))
 
-# Entradas
-entrada = input()
-
-# Proceso
-
-
-# Salidas
-print(salida)
+#Proceso
+if numero_1 % numero_2 == 0:
+    print(f"El número {numero_1} es múltiplo del número {numero_2}")
+elif numero_2 % numero_1 == 0:
+    print(f"El número {numero_2} es múltiplo del número {numero_1}")
+else: 
+    print("Ninguno de los números es múltiplo del otro")
