0a380d9dfc9023e7b7e9c70b65dd81968d90254c
Priscr23 <Priscila.cervantes@iteso.mx>
2023-10-12 11:04:15 -0600

    if

--- multiplos.py
+++ multiplos.py
@@ -10,7 +10,7 @@ numero_2 = int(input("Introduzca otro número: "))
 #Proceso
 if numero_1 % numero_2 == 0:
     print(f"El número {numero_1} es múltiplo del {numero_2}")
-elif numero_2 % numero_1 == 0:
+if numero_2 % numero_1 == 0:
     print(f"El número {numero_2} es múltiplo del {numero_1}")
 else: 
     print(f"Ninguno de los números es múltiplo del otro")
